03a15642bb2240cd01996d99ceb5e7992faf1941
yixinglu <2520865+yixinglu@users.noreply.github.com>
2021-11-22 14:57:07 +0800

    Remove stale codes

--- main.py
+++ main.py
@@ -103,8 +103,6 @@ def apply_patch(branch, comm_ci, comm_repo):
     git.clean("-f")
     git.fetch("origin", "master")
     git.checkout("-b", branch, "origin/master")
-    git.remote('add', 'community', 'git@github.com:{}.git'.format(comm_repo.full_name))
-    git.fetch('community', 'master')
     git_commit = comm_ci.commit
     try:
         git.remote('add', 'community', 'https://github.com/{}.git'.format(comm_repo.full_name))
